e8e9ac0c803898e71f10f46a3e9f96f3c00d9ccb
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-10-18 14:14:13 +0300

    update read.me

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine(solution.MinimumTime(3, new int[][]
 	new int[] { 1, 3 }, 
 	new int[] { 2, 3 }
 }, new int[] { 3, 2, 5 }));
-Console.WriteLine(solution.MinimumTime(3, new int[][]
+Console.WriteLine(solution.MinimumTime(5, new int[][]
 {
 	new int[] { 1, 5 },
 	new int[] { 2, 5 },
@@ -17,7 +17,21 @@ public class Solution
 {
 	private int DFS(List<int>[] graph, Dictionary<int, int> dp, int[] times, int node)
 	{
-
+		if (dp.ContainsKey(node))
+		{
+			return dp[node];
+		}
+		if (graph[node].Count == 0)
+		{
+			return times[node];
+		}
+		int maximumTime = 0;
+		foreach (int child in graph[node])
+		{
+			maximumTime = Math.Max(maximumTime, DFS(graph, dp, times, child));
+		}
+		dp[node] = maximumTime + times[node];
+		return dp[node];
 	}
 
 	public int MinimumTime(int n, int[][] relations, int[] time)
@@ -28,6 +42,17 @@ public class Solution
 		{
 			graph[node] = new List<int>();
 		}
-		return -1;
+		foreach (int[] relation in relations)
+		{
+			int prevCourse = relation[0] - 1;
+			int curCourse = relation[1] - 1;
+			graph[curCourse].Add(prevCourse);
+		}
+		int minimumTime = 0;
+		for (int node = 0; node < n; ++node)
+		{
+			minimumTime = Math.Max(minimumTime, DFS(graph, dp, time, node));
+		}
+		return minimumTime;
 	}
 }
